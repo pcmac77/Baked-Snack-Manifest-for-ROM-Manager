6cfd3f5ba73ddbd258e66ac5aaa4621d44864f4e
Peter Chuang <pcmac@peterchuang.com>
2010-08-31 23:51:34 -0500

    initial commit for BS 1.5

---
+++ bs_manifest.js
@@ -0,0 +1,34 @@
+{
+    // The version of the manifest, so ROM Manager knows how to load it
+    version: 1,
+    // The homepage for your ROM, if you have one.
+    homepage: "http://www.bakedsnackshack.com/",
+    // The donate link for yuor ROM, if you have one.
+    donate: "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3J28MNKNUWVP2",
+    
+    // Your list of ROMs
+    roms:
+    [
+	// =====Baked Snack 1.5
+    {
+        // Display name of the rom
+        name: "Baked Snack 1.5",
+        // Display summary of the rom
+        summary: "Stable",
+        // Device that this rom runs on
+        device: "supersonic",
+        // A screenshot to show off what the ROM looks like! (This is optional)
+        //screenshot: "http://www.droidaftermarket.com/koush//motorola/sholes/screenshot2.png",
+        // This is the download URL of your ROM.
+        // These URLs should have unique filenames per version!
+        // Ie, don't use a zip with the same name
+        // as that of a previous version with a new version. 
+        // Otherwise ROM Manager will continue using the
+        // old cached zip on the SD card.
+        // This must be a direct download,
+        // and not a Mediafire type link.
+        url: "http://www.bakedsnackshack.com/ROMS/BakedSnack150.zip"
+		]
+    }
+    ]
+}
