cb464153d1103a051fbbd7148e86ecccb90ec283
Peter Chuang <pcmac@peterchuang.com>
2011-01-01 11:48:08 -0600

    add BS 1.9

--- bs_manifest.js
+++ bs_manifest.js
@@ -4,6 +4,27 @@
     donate: "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3J28MNKNUWVP2",
     roms:
     [
+    // =====Baked Snack 1.9 EVO
+    {
+        name: "Baked Snack 1.9",
+        summary: "",
+        device: "supersonic",
+        product: "BakedSnack",
+        incremental: 1900,
+        modversion: "1",
+        screenshots:
+        [
+            "http://img525.imageshack.us/img525/3415/homeao.png",
+            "http://img180.imageshack.us/img180/1316/7home.png",
+            "http://img683.imageshack.us/img683/4823/apps1f.png"
+        ],
+        urls: 
+        [
+            "http://www.bakedsnackshack.com/ROMS/EVO/BakedSnack1.9.evo.zip"
+        ]
+    }
+    ],
+    [
     // =====Baked Snack 1.8 EVO
     {
         name: "Baked Snack 1.8",
