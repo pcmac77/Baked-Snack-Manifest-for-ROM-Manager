a331eadfacb8c69257cfa7232de2177a9ea9ea29
Peter Chuang <pcmac@peterchuang.com>
2011-01-01 11:19:02 -0600

    revert brackets

--- bs_manifest.js
+++ bs_manifest.js
@@ -18,23 +18,9 @@
             "http://img180.imageshack.us/img180/1316/7home.png",
             "http://img683.imageshack.us/img683/4823/apps1f.png"
         ],
-        urls: "http://www.bakedsnackshack.com/ROMS/EVO/BakedSnack1.8.evo.zip"
-    },
-    // =====Baked Snack 1.75
-    {
-        name: "Baked Snack 1.75",
-        summary: "Flash Update, 1.7 Required.  Optional add-ons available.",
-        device: "supersonic",
-        product: "BakedSnack",
-        incremental: 1750,
-        modversion: "1",
-        screenshots:
+        urls: 
         [
-            "http://img525.imageshack.us/img525/3415/homeao.png",
-            "http://img180.imageshack.us/img180/1316/7home.png",
-            "http://img683.imageshack.us/img683/4823/apps1f.png"
-        ],
-        url: "http://www.bakedsnackshack.com/ROMS/EVO/1.75.zip"
+            "http://www.bakedsnackshack.com/ROMS/EVO/BakedSnack1.8.evo.zip"
+        ]
     }
-    ]
 }
